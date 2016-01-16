c129c994640f3486031a2095a7d06c4f6ce081a2
footvaalvica <mateusleitepinho@gmail.com>
2016-01-16 09:02:11 +0000

    Updated simfin.py
    Removed some comments

--- simfin.py
+++ simfin.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3.4
-
-#imports the simfin.json module (needed to write and read the json file).
 import json
 
-#Opens the simfin.json file for loading the variable.
 open('simfin.json').read()
 var = json.loads(open('simfin.json').read())
 var = var["var"]
 
-#Asks the user if he wants to add or to remove money.
 print("What do you want to do?")
 x = input()
 
