b743955c2bd3104bdd06b0cc44b3d28ffdf29fe7
footvaalvica <mateusleitepinho@gmail.com>
2016-01-16 15:50:40 +0000

    Update simfin.py
    Added simple history system

--- simfin.py
+++ simfin.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
+
 import json
+import time
 
 open('simfin.json').read()
 var = json.loads(open('simfin.json').read())
 var = var["var"]
+moneyaddrm = json.loads(open('simfin.json').read()) 
+moneyaddrm = moneyaddrm["moneyaddrm"]
+whenaddrm = json.loads(open('simfin.json').read())
+whenaddrm = whenaddrm["whenaddrm"]
 
 print("What do you want to do?")
 x = input()
+
+if x == 'history':
+	print("Last time you added/removed %s€." %(moneyaddrm))
+	print("at %s" %(whenaddrm)) 
 
 if x == 'howmuch':
 	print("You currently have %s" %(var))
@@ -17,7 +27,7 @@ if x == 'add':
 	print ("You now have %s" %(var+y))
 	var = (var+y)
 	with open('simfin.json', 'w') as outfile:
-		json.dump({'var':int(var)}, outfile, indent=4)
+		json.dump({'var':int(var), 'moneyaddrm':int(y), 'whenaddrm':(time.strftime("%d/%m/%Y %H:%M")) }, outfile, indent=4)
 
 if x == 'remove':
 	print("How much money do you want to remove? ")
@@ -25,5 +35,5 @@ if x == 'remove':
 	print ("You now have %s" %(var-y))
 	var = (var-y)
 	with open('simfin.json' , 'w') as outfile:
-		json.dump({'var':int(var)}, outfile, indent=4)
+		json.dump({'var':int(var), 'moneyaddrm':int(y), 'whenaddrm':(time.strftime("%d/%m/%Y %H:%M")) }, outfile, indent=4)
 		
