85052b60729593d2a105c2f65108be7308e8458a
Mateus Pinho <mateusleitepinho@gmail.com>
2016-01-31 12:21:13 +0000

    I added error handling that kind of works, it dosen't allow you to input anything that's not a number

--- simfin.py
+++ simfin.py
@@ -2,16 +2,11 @@
 
 #TODO's:
 #separate the different actions (add, remove etc.) to functions/methods
-#add error handling with exceptions (what if someone inserts non-int values to the dialog of how much money one inserts/removes)
 #remove possibility to go to negative funds, unless you implement a credit system of some sort.
 #perhaps encapsulate your "bank" account as a class, so that you don't end up with a metric fuckton of variables. Also good practice for learning.
 
 import json
 import time
-
-def writeout():
-	with open('simfin.json' , 'w') as outfile:
-		json.dump({'var':int(var), 'moneyaddrm':int(y), 'whenaddrm':(time.strftime("%d/%m/%Y %H:%M")) }, outfile, indent=4)
 
 open('simfin.json').read()
 var = json.loads(open('simfin.json').read())
@@ -21,28 +16,59 @@ moneyaddrm = moneyaddrm["moneyaddrm"]
 whenaddrm = json.loads(open('simfin.json').read())
 whenaddrm = whenaddrm["whenaddrm"]
 
+def history():
+	print("Last time you added/removed %s." %(moneyaddrm))
+	print("at %s" %(whenaddrm))
+
+def howmuch():
+	print("You currently have %s" %(var))
+
+def remove():
+	global var
+	print("How much money do you want to remove? ")
+	y = input()
+	if y.isnumeric() == False:
+		print("That's not a valid number!")
+		y = 0
+		remove()
+	z = int(y)
+	global lz
+	lz = z
+	print ("You now have %s" %(var-lz))
+	var = (var-lz)
+	writeout()
+
+def writeout():
+	global lz
+	with open('simfin.json' , 'w') as outfile:
+		json.dump({'var':var, 'moneyaddrm':lz, 'whenaddrm':(time.strftime("%d/%m/%Y %H:%M")) }, outfile, indent=4)
+
+def add():
+	global var
+	print("How much money do you want to add? ")
+	y = input()
+	if y.isnumeric() == False:
+		print("That's not a valid number!")
+		y = 0
+		add()
+	z = int(y)
+	global lz
+	lz = z
+	print ("You now have %s" %(var+lz))
+	var = (var+lz)
+	writeout()
+
 print("What do you want to do?")
 x = input()
 
 if x == 'history':
-	print("Last time you added/removed %s." %(moneyaddrm))
-	print("at %s" %(whenaddrm))
+	history()
 
 if x == 'howmuch':
-	print("You currently have %s" %(var))
+	howmuch()
 
 if x == 'add':
-	print("How much money do you want to add? ")
-	y = int(input())
-	print ("You now have %s" %(var+y))
-	var = (var+y)
-	writeout()
-
+	add()
 
 if x == 'remove':
-	print("How much money do you want to remove? ")
-	y = int(input())
-	print ("You now have %s" %(var-y))
-	var = (var-y)
-	writeout()
-
+	remove()
