807f6c148b62b2df4c11679c8a27f601ff92fd9f
Mateus Pinho <mateusleitepinho@gmail.com>
2017-10-29 11:32:41 +0000

    Changed code to fit the Python Code Style Guidelines

--- simfin.py
+++ simfin.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
-#TODO's:
-#remove possibility to go to negative funds, unless you implement a credit system of some sort.
-#perhaps encapsulate your "bank" account as a class, so that you don't end up with a metric fuckton of variables.
-#Also good practice for learning.
+# TODO's:
+
+# remove possibility to go to negative funds, unless you implement a credit system of some sort.
+
+# perhaps encapsulate your "bank" account as a class, so that you don't end up with a metric fuckton of variables.
+
+# Also good practice for learning.
 
 import json
 import time
@@ -16,79 +19,87 @@ moneyaddrm = moneyaddrm["moneyaddrm"]
 whenaddrm = json.loads(open('simfin.json').read())
 whenaddrm = whenaddrm["whenaddrm"]
 
+
 def history():
-	print("Last time you added/removed %s." %(moneyaddrm))
-	print("at %s" %(whenaddrm))
+    print("Last time you added/removed %s." % (moneyaddrm))
+    print("at %s" % (whenaddrm))
+
 
 def howmuch():
-	print("You currently have %s" %(var))
+    print("You currently have %s" % (var))
+
 
 def remove():
-	global var
-	print("How much money do you want to remove? ")
-	y = input()
-	if y.isnumeric() == False:
-		print("That's not a valid number!")
-		y = 0
-		remove()
-	z = int(y)
-	global lz
-	lz = z
-	print ("You now have %s" %(var-lz))
-	var = (var-lz)
-	writeout()
+    global var
+    print("How much money do you want to remove? ")
+    y = input()
+    if y.isnumeric() is False:
+        print("That's not a valid number!")
+        y = 0
+        remove()
+    z = int(y)
+    global lz
+    lz = z
+    print("You now have %s" % (var - lz))
+    var = (var - lz)
+    writeout()
+
 
 def writeout():
-	global lz
-	with open('simfin.json' , 'w') as outfile:
-		json.dump({'var':var, 'moneyaddrm':lz, 'whenaddrm':(time.strftime("%d/%m/%Y %H:%M")) }, outfile, indent=4)
+    global lz
+    with open('simfin.json', 'w') as outfile:
+        json.dump({'var': var, 'moneyaddrm': lz, 'whenaddrm': (time.strftime("%d/%m/%Y %H:%M"))}, outfile, indent=4)
+
 
 def add():
-	global var
-	print("How much money do you want to add? ")
-	y = input()
-	if y.isnumeric() == False:
-		print("That's not a valid number!")
-		y = 0
-		add()
-	z = int(y)
-	global lz
-	lz = z
-	print ("You now have %s" %(var+lz))
-	var = (var+lz)
-	writeout()
+    global var
+    print("How much money do you want to add? ")
+    y = input()
+    if y.isnumeric() is False:
+        print("That's not a valid number!")
+        y = 0
+        add()
+    z = int(y)
+    global lz
+    lz = z
+    print("You now have %s" % (var + lz))
+    var = (var + lz)
+    writeout()
+
 
 def help():
-	print("add for adding money")
-	print("howmuch to check money amount")
-	print("history to check when you last added/removed money")
-	print("remove to remove money")
-	print("close to close the program")
-	print("and help to run this again")
-	start()
+    print("add for adding money")
+    print("howmuch to check money amount")
+    print("history to check when you last added/removed money")
+    print("remove to remove money")
+    print("close to close the program")
+    print("and help to run this again")
+    start()
+
 
 def start():
-	print("What do you want to do?")
-	x = input()
-	if x == 'history':
-		history()
-	
-	elif x == 'howmuch':
-		howmuch()
-	
-	elif x == 'add':
-		add()
-	
-	elif x == 'remove':
-		remove()
-	
-	elif x == 'help':
-		help()
+    print("What do you want to do?")
+    x = input()
+    if x == 'history':
+        history()
 
-	elif x == 'close':
-		pass
+    elif x == 'howmuch':
+        howmuch()
 
-	else:
-		help()
+    elif x == 'add':
+        add()
+
+    elif x == 'remove':
+        remove()
+
+    elif x == 'help':
+        help()
+
+    elif x == 'close':
+        pass
+
+    else:
+        help()
+
 
 start()
